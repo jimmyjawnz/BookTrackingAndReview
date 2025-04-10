afbe30e82401262290fce6983061c6f7071bcf11
davidblackwell121 <david.blackwell121@gmail.com>
2025-04-09 23:55:03 -0600

    id is now grabbed during sign in

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,6 +8,7 @@ import Browse from './components/Browse';
 import './App.css';
 import BookDetails from './components/BookDetails'; 
 import UserProfile from './components/UserProfile';
+import ProtectedRoute from './components/ProtectedRoute';
 
 
 function App() {
@@ -17,14 +18,17 @@ function App() {
                       <Navbar />
         
         <Routes> 
-        <Route path="/book/:id" element={<BookDetails />} />
+          <Route path="/book/:id" element={<BookDetails />} />
 
           <Route path="/" element={<Home />} />
           <Route path="/browse" element={<Browse />} />
           <Route path="/register" element={<RegisterForm />} />
           <Route path="/signin" element={<SigninForm />} />
-          <Route path="/user" element={<UserProfile />} />
-
+          <Route path="/user" element={
+            <ProtectedRoute>
+              <UserProfile />
+            </ProtectedRoute>
+          } />
         </Routes>
       </div>
     </Router>
---
+++ frontend/src/components/ProtectedRoute.jsx
@@ -0,0 +1,11 @@
+import { Navigate } from 'react-router-dom';
+import { useAuth } from '../components/AuthContext';
+
+const ProtectedRoute = ({ children }) => {
+  const { user } = useAuth();
+
+  if (!user) return <Navigate to="/signin" replace />;
+  return children;
+};
+
+export default ProtectedRoute;
